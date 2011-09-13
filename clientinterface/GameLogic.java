9f75171b96ef0180942486c2b6617b7768f2d897
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-13 00:33:18 +0000

    Re-organized some packages and files, added a PlayerMessage argument to GameLogic.performActionOn

--- clientinterface/GameLogic.java
+++ clientinterface/GameLogic.java
@@ -1,7 +1,9 @@
-package client;
+package clientinterface;
+
+import game.PlayerMessage;
 
 // MaxZ's note: I'd imagine performining an action on something would be an operation on the object,
 //  in OOP.
 public interface GameLogic {
-	public void performActionOn(String action, GameThing object);
+	public void performActionOn(String action, GameThing object, PlayerMessage who);
 }
--- game/PlayerMessage.java
+++ game/PlayerMessage.java
@@ -1,6 +1,7 @@
+package game;
 import java.util.List;
 
 public interface PlayerMessage {
 	public void logMessage(String message);
-	public void showContainer(String name, List<client.GameThing> contents);
+	public void showContainer(String name, List<clientinterface.GameThing> contents);
 }
