3b1a65cdb9427824567fd17147b9a75d35641cb6
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-17 02:57:57 +0000

    Added: IsoInterface which manages the entire interface

---
+++ ui/isometric/IsoInterface.java
@@ -0,0 +1,48 @@
+package ui.isometric;
+
+import java.util.List;
+
+import game.PlayerMessage;
+
+import javax.swing.JFrame;
+
+import clientinterface.GameModel;
+import clientinterface.GameThing;
+
+public class IsoInterface implements PlayerMessage {
+	private JFrame frame;
+	
+	/**
+	 * Create a interface with a given GameModel
+	 * @param name
+	 * @param model
+	 */
+	public IsoInterface(String name, GameModel model) {
+		frame = new JFrame(name);
+		IsoDataSource d = new IsoTestDataSource(); // todo, change
+		IsoCanvas canvas = new IsoCanvas(d);
+		canvas.setSize(300, 300);
+		canvas.repaint();
+		frame.add(canvas);
+		frame.pack();
+	}
+	
+	/**
+	 * Display this interface
+	 */
+	public void show() {
+		frame.setVisible(true);
+	}
+
+	@Override
+	public void logMessage(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void showContainer(String name, List<GameThing> contents) {
+		// TODO Auto-generated method stub
+		
+	}
+}
--- ui/isometric/UITestMain.java
+++ ui/isometric/UITestMain.java
@@ -1,16 +1,8 @@
 package ui.isometric;
-import javax.swing.JFrame;
-
 
 public class UITestMain {
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("IsoTest");
-		IsoDataSource d = new IsoTestDataSource();
-		IsoCanvas canvas = new IsoCanvas(d);
-		canvas.setSize(300, 300);
-		canvas.repaint();
-		frame.add(canvas);
-		frame.pack();
-		frame.setVisible(true);
+		IsoInterface view = new IsoInterface("IsoTest", null);
+		view.show();
 	}
 }
