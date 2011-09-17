f5a5cad51c7616d2df8903ec76b1f4b0b8284af3
Max Zerzouri <maxdamantus@gmail.com>
2011-09-17 05:08:14 +0000

    A test world with one tile, at (5, 4)

--- ui/isometric/UITestMain.java
+++ ui/isometric/UITestMain.java
@@ -1,8 +1,15 @@
 package ui.isometric;
 
+import util.*;
+import clientinterface.*;
+
 public class UITestMain {
+	private static game.GameModel sgm = new game.GameModel();
+
 	public static void main(String[] args) {
-		IsoInterface view = new IsoInterface("IsoTest", null);
+		game.GameThing tile = new game.things.GroundTile(sgm);
+		sgm.level(0).put(new Position(5, 4), tile);
+		IsoInterface view = new IsoInterface("IsoTest", Conversions.fromServerGameModel(sgm));
 		view.show();
 	}
 }
