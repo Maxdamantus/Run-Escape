21100454053e0f0e264ecd77bfab535250b675c8
Max Zerzouri <maxdamantus@gmail.com>
2011-09-17 03:00:35 +0000

    Renamed rotation -> direction

--- clientinterface/GameThing.java
+++ clientinterface/GameThing.java
@@ -6,11 +6,11 @@ import java.util.*;
 
 public interface GameThing {
 	public Position position();
-	// relative to the position; should not change during the lifetime of the Thing
+	public Direction direction();
+	// relative to the position and direction; should not change during the lifetime of the Thing
 	public Area area();
 	public String renderer();
 	public int gid();
 	public List<String> interactions();
 	public Map<String, Map<String, Object>> userArguments();
-	public Direction rotation();
 }
