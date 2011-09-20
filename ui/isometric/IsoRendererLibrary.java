5fd5c8f21ca35e21b410312b546e8a839df94f2e
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-20 13:06:24 +0000

    IsoRenderLibrary: Renamed loadImageAll to loadImage1 to match the other methods better

--- ui/isometric/IsoRendererLibrary.java
+++ ui/isometric/IsoRendererLibrary.java
@@ -31,53 +31,53 @@ public class IsoRendererLibrary {
 			if(renderers == null) {
 				renderers = new HashMap<String, Map<Direction, BufferedImage>>();
 				
-				renderers.put("ground_grey_1", loadImageAll("ground_grey_1"));
-				renderers.put("ground_grey_2", loadImageAll("ground_grey_2"));
+				renderers.put("ground_grey_1", loadImage1("ground_grey_1"));
+				renderers.put("ground_grey_2", loadImage1("ground_grey_2"));
 				
-				renderers.put("ground_grey_dark_circle_1", loadImageAll("ground_grey_dark_circle_1"));
+				renderers.put("ground_grey_dark_circle_1", loadImage1("ground_grey_dark_circle_1"));
 				
-				renderers.put("ground_grey_dark_dots_1", loadImageAll("ground_grey_dark_dots_1"));
-				renderers.put("ground_grey_green_dots_1", loadImageAll("ground_grey_green_dots_1"));
-				renderers.put("ground_grey_red_dots_1", loadImageAll("ground_grey_red_dots_1"));
+				renderers.put("ground_grey_dark_dots_1", loadImage1("ground_grey_dark_dots_1"));
+				renderers.put("ground_grey_green_dots_1", loadImage1("ground_grey_green_dots_1"));
+				renderers.put("ground_grey_red_dots_1", loadImage1("ground_grey_red_dots_1"));
 				
-				renderers.put("ground_grey_greenish_1", loadImageAll("ground_grey_greenish_1"));
-				renderers.put("ground_grey_greenish_2", loadImageAll("ground_grey_greenish_2"));
+				renderers.put("ground_grey_greenish_1", loadImage1("ground_grey_greenish_1"));
+				renderers.put("ground_grey_greenish_2", loadImage1("ground_grey_greenish_2"));
 				
-				renderers.put("ground_grey_mushrooms_1", loadImageAll("ground_grey_mushrooms_1"));
-				renderers.put("ground_grey_mushrooms_2", loadImageAll("ground_grey_mushrooms_2"));
-				renderers.put("ground_grey_mushrooms_3", loadImageAll("ground_grey_mushrooms_3"));
-				renderers.put("ground_grey_mushrooms_4", loadImageAll("ground_grey_mushrooms_4"));
-				renderers.put("ground_grey_mushrooms_5", loadImageAll("ground_grey_mushrooms_5"));
-				renderers.put("ground_grey_mushrooms_6", loadImageAll("ground_grey_mushrooms_6"));
-				renderers.put("ground_grey_mushrooms_7", loadImageAll("ground_grey_mushrooms_7"));
-				renderers.put("ground_grey_mushrooms_8", loadImageAll("ground_grey_mushrooms_8"));
+				renderers.put("ground_grey_mushrooms_1", loadImage1("ground_grey_mushrooms_1"));
+				renderers.put("ground_grey_mushrooms_2", loadImage1("ground_grey_mushrooms_2"));
+				renderers.put("ground_grey_mushrooms_3", loadImage1("ground_grey_mushrooms_3"));
+				renderers.put("ground_grey_mushrooms_4", loadImage1("ground_grey_mushrooms_4"));
+				renderers.put("ground_grey_mushrooms_5", loadImage1("ground_grey_mushrooms_5"));
+				renderers.put("ground_grey_mushrooms_6", loadImage1("ground_grey_mushrooms_6"));
+				renderers.put("ground_grey_mushrooms_7", loadImage1("ground_grey_mushrooms_7"));
+				renderers.put("ground_grey_mushrooms_8", loadImage1("ground_grey_mushrooms_8"));
 				
-				renderers.put("ground_grey_patch_1", loadImageAll("ground_grey_patch_1"));
+				renderers.put("ground_grey_patch_1", loadImage1("ground_grey_patch_1"));
 				
-				renderers.put("ground_grey_pool_1", loadImageAll("ground_grey_pool_1"));
-				renderers.put("ground_grey_pools_1", loadImageAll("ground_grey_pools_1"));
-				renderers.put("ground_grey_pools_2", loadImageAll("ground_grey_pools_2"));
+				renderers.put("ground_grey_pool_1", loadImage1("ground_grey_pool_1"));
+				renderers.put("ground_grey_pools_1", loadImage1("ground_grey_pools_1"));
+				renderers.put("ground_grey_pools_2", loadImage1("ground_grey_pools_2"));
 				
-				renderers.put("ground_grey_rock_1", loadImageAll("ground_grey_rock_1"));
-				renderers.put("ground_grey_rock_2", loadImageAll("ground_grey_rock_2"));
-				renderers.put("ground_grey_rock_3", loadImageAll("ground_grey_rock_3"));
+				renderers.put("ground_grey_rock_1", loadImage1("ground_grey_rock_1"));
+				renderers.put("ground_grey_rock_2", loadImage1("ground_grey_rock_2"));
+				renderers.put("ground_grey_rock_3", loadImage1("ground_grey_rock_3"));
 				
-				renderers.put("ground_grey_spikes_1", loadImageAll("ground_grey_spikes_1"));
-				renderers.put("ground_grey_spikes_2", loadImageAll("ground_grey_spikes_2"));
-				renderers.put("ground_grey_spikes_3", loadImageAll("ground_grey_spikes_3"));
+				renderers.put("ground_grey_spikes_1", loadImage1("ground_grey_spikes_1"));
+				renderers.put("ground_grey_spikes_2", loadImage1("ground_grey_spikes_2"));
+				renderers.put("ground_grey_spikes_3", loadImage1("ground_grey_spikes_3"));
 				
-				renderers.put("ground_grey_stones_1", loadImageAll("ground_grey_stones_1"));
+				renderers.put("ground_grey_stones_1", loadImage1("ground_grey_stones_1"));
 				
-				renderers.put("ground_grey_trash_1", loadImageAll("ground_grey_trash_1"));
+				renderers.put("ground_grey_trash_1", loadImage1("ground_grey_trash_1"));
 				
-				renderers.put("ground_grey_water_island_1", loadImageAll("ground_grey_water_island_1"));
-				renderers.put("ground_grey_water_rock_1", loadImageAll("ground_grey_water_rock_1"));
+				renderers.put("ground_grey_water_island_1", loadImage1("ground_grey_water_island_1"));
+				renderers.put("ground_grey_water_rock_1", loadImage1("ground_grey_water_rock_1"));
 				
 				renderers.put("ground_grey_water_corner", loadImage4("ground_grey_water_corner"));
 				renderers.put("ground_grey_water_one_side", loadImage4("ground_grey_water_one_side"));
 				renderers.put("ground_grey_water_two_sides", loadImage4("ground_grey_water_two_sides"));
 				
-				renderers.put("water_1", loadImageAll("water_1"));
+				renderers.put("water_1", loadImage1("water_1"));
 				
 				
 				
@@ -88,14 +88,14 @@ public class IsoRendererLibrary {
 				
 				renderers.put("ground_grey_road_t_1", loadImage4("ground_grey_road_t_1"));
 				
-				renderers.put("ground_grey_road_x_1", loadImageAll("ground_grey_road_x_1"));
+				renderers.put("ground_grey_road_x_1", loadImage1("ground_grey_road_x_1"));
 				
 				renderers.put("ground_grey_tile_1_corner_1", loadImage4("ground_grey_tile_1_corner_1"));
 				renderers.put("ground_grey_tile_1_one_side_1", loadImage4("ground_grey_tile_1_one_side_1"));
 				renderers.put("ground_grey_tile_1_two_sides_1", loadImage4("ground_grey_tile_1_two_sides_1"));
 				
-				renderers.put("ground_tile_1_greenish_1", loadImageAll("ground_tile_1_greenish_1"));
-				renderers.put("ground_tile_1", loadImageAll("ground_tile_1"));
+				renderers.put("ground_tile_1_greenish_1", loadImage1("ground_tile_1_greenish_1"));
+				renderers.put("ground_tile_1", loadImage1("ground_tile_1"));
 				
 				renderers.put("character_cordi_empty", loadImage4("character_cordi_empty"));
 			}
@@ -108,7 +108,7 @@ public class IsoRendererLibrary {
 	 * @param resourceName
 	 * @return
 	 */
-	private static Map<Direction, BufferedImage> loadImageAll(String resourceName) {
+	private static Map<Direction, BufferedImage> loadImage1(String resourceName) {
 		Map<Direction, BufferedImage> map = new HashMap<Direction, BufferedImage>();
 		
 		BufferedImage image = Resources.readImageResourceUnfliped("/resources/isotiles/"+resourceName+".png");
