0988c905d5fef6ccd49d97d477d84a61a1ac0f80
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-24 05:23:45 +0000

    UITestMain works again after MaxZs refactor

--- ui/isometric/mock/UITestMain.java
+++ ui/isometric/mock/UITestMain.java
@@ -13,7 +13,7 @@ public class UITestMain {
 		sgm.level(0).put(new Position(5, 1), new game.things.GroundTile(sgm, "ground_grey_water_two_sides"));
 		//sgm.level(0).put(new Position(5, 2), new game.things.Wall(sgm, "wallx"));
 		Position pos51 = new Position(5,1);
-		sgm.level(0).put(pos51, new game.things.Player(sgm, pos51, Direction.SOUTH, "character_cordi_empty"));
+//		sgm.level(0).put(pos51, new game.things.Player(sgm, pos51, Direction.SOUTH, "character_cordi_empty"));
 		IsoInterface view = new IsoInterface("IsoTest", Conversions.fromServerGameModel(sgm), new IsoGameLogicMock());
 		view.show();
 	}
