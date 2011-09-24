121e6ac50eb4ed2cfde0cf6b907363a87ba9bee4
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-24 07:50:29 +0000

    Fixed errors/warnings

--- server/Main.java
+++ server/Main.java
@@ -144,8 +144,8 @@ public class Main{
 		game.GameThing tile = new game.things.GroundTile(sgm);
 		sgm.level(0).put(new Position(5, 0), tile);
 		sgm.level(0).put(new Position(5, 1), new game.things.GroundTile(sgm, "ground_grey_water_two_sides"));
-		Position pos51 = new Position(5,1);
-		sgm.level(0).put(pos51, new game.things.Player(sgm, pos51, Direction.SOUTH, "character_cordi_empty"));
+//		Position pos51 = new Position(5,1);
+//		sgm.level(0).put(pos51, new game.things.Player(sgm, pos51, Direction.SOUTH, "character_cordi_empty"));
 		return sgm;
 	}
 		
