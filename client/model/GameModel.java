90d4bed479b13f2e6830ee6a8bbab1a0d3ed0d18
greenwthom <greenwthom@ecs.vuw.ac.nz>
2011-09-24 04:48:59 +0000

    added requests for  client.model.GameModel methods in comments

--- client/model/GameModel.java
+++ client/model/GameModel.java
@@ -5,4 +5,10 @@ import util.*;
 public interface GameModel {
 	public Iterable<GameThing> thingsInRect(Area a);
 	public GameThing thingWithGID(int gid);
+	
+	//please put these in, thanks, tom
+	
+	public void updateModel (serialization.Tree tree);
+	
+	//in the class, please put a constructor that takes an update tree as a parameter 
 }
