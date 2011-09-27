e36201aa38cb5fb1288fd9261ed8c1c40c12ad9e
Max Zerzouri <maxdamantus@gmail.com>
2011-09-27 08:17:23 +0000

    Moved GameLogic back in

---
+++ game/GameLogic.java
@@ -0,0 +1,7 @@
+package game;
+
+// MaxZ's note: I'd imagine performining an action on something would be an operation on the object,
+//  in OOP.
+public interface GameLogic {
+	public void performActionOn(String action, GameThing object);
+}
