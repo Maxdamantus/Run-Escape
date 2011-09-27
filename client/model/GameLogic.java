9aac03254a5878cad2a6f6d3be236d67a58cbaff
Max Zerzouri <maxdamantus@gmail.com>
2011-09-27 07:11:56 +0000

    Moving obsolete client.model out of the way.

--- client/model/GameLogic.java
+++
@@ -1,9 +0,0 @@
-package client.model;
-
-import game.PlayerMessage;
-
-// MaxZ's note: I'd imagine performining an action on something would be an operation on the object,
-//  in OOP.
-public interface GameLogic {
-	public void performActionOn(String action, GameThing object);
-}
