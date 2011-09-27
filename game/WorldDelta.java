3378d93feca5470ac471d473e20be465212d3c21
Max Zerzouri <maxdamantus@gmail.com>
2011-09-27 07:44:25 +0000

    game package compiling .. doubtful of anything else

--- game/WorldDelta.java
+++ game/WorldDelta.java
@@ -5,8 +5,8 @@ import serialization.util.Serializers;
 
 public class WorldDelta {
 	public static interface Action {
-		public void apply(GameWorld world);
-		public Tree toTree();
+		public void apply(GameWorld w);
+		public Tree toTree(GameWorld w);
 		public String type();
 	}
 
@@ -22,28 +22,25 @@ public class WorldDelta {
 			loc.put(world.thingWithGID(gid));
 		}
 
-		public static Serializer<Put> SERIALIZER = new Serializer<Put>(){
-			public Tree write(/* Vladimir */ Put in){
-				Tree out = new Tree();
-				out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
-				out.add(new Tree.Entry("location", Location.WRITER.write(in.loc)));
-				return out;
-			}
+		public static Serializer<Put> serializer(final GameWorld w){
+			return new Serializer<Put>(){
+				public Tree write(/* Vladimir */ Put in){
+					Tree out = new Tree();
+					out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
+					out.add(new Tree.Entry("location", LocationS.s(w).write(in.loc)));
+					return out;
+				}
 
-			public Put read(Tree in){
-				int gid = -1;
-				client.model.Location loc = null;
-				for(Tree.Entry te : in.children())
-					if(te.name().equals("gid"))
-						gid = Serializers.Serializer_Integer.read(te.tree());
-					else if(te.name().equals("location"))
-						loc = client.model.Location.READER.read(te.tree());
-				return new Put(gid, loc);
-			}
-		};
+				public Put read(Tree in){
+					return new Put(
+						Serializers.Serializer_Integer.read(in.find("gid")),
+						LocationS.s(w).read(in.find("location")));
+				}
+			};
+		}
 
-		public Tree toTree(){
-			return SERIALIZER.write(this);
+		public Tree toTree(GameWorld w){
+			return serializer(w).write(this);
 		}
 
 		public String type(){
@@ -57,24 +54,25 @@ public class WorldDelta {
 		action = a;
 	}
 
-	public final static Serializer<WorldDelta> SERIALIZER = new Serializer<WorldDelta>(){
-		public Tree write(WorldDelta in){
-			Tree out = new Tree();
-			out.add(new Tree.Entry("type", new Tree(in.action.type())));
-			out.add(new Tree.Entry("action", in.action.serialiser().write(action)));
+	public final static Serializer<WorldDelta> serializer(final GameWorld w){
+		return new Serializer<WorldDelta>(){
+			public Tree write(WorldDelta in){
+				Tree out = new Tree();
+				out.add(new Tree.Entry("type", new Tree(in.action.type())));
+				out.add(new Tree.Entry("action", in.action.toTree(w)));
+				return out;
+			}
 
-			return out;
-		}
-
-		public WorldDelda read(Tree in){
-			Serializer<Action> as = null;
-			for(Tree.Entry te : in.children())
-				if(te.name().equals("type")){
-					if(te.tree().value().equals("put"))
-						as = Put.SERIALIZER;
-				}else if(te.name().equals("action"))
-					return as.read(te.tree());
-			throw new RuntimeException("wtf");
-		}
-	};
+			public WorldDelta read(Tree in){
+				Reader<? extends Action> as = null;
+				for(Tree.Entry te : in.children())
+					if(te.name().equals("type")){
+						if(te.tree().value().equals("put"))
+							as = Put.serializer(w);
+					}else if(te.name().equals("action"))
+						return new WorldDelta(as.read(te.tree()));
+				throw new RuntimeException("wtf");
+			}
+		};
+	}
 }
