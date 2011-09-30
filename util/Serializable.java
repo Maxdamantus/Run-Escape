74b3e973f26b1f973c17464da1549f659d0f71b3
Melby Ruarus <melby.ruarus@gmail.com>
2011-09-30 02:10:57 +0000

    Added more into for write

--- util/Serializable.java
+++ util/Serializable.java
@@ -1,20 +1,30 @@
 package util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import serialization.Serializer;
+import serialization.Serializers;
 import serialization.Tree;
 
 public interface Serializable<T> {
 	@SuppressWarnings("rawtypes")
-	Serializer<Serializable> serializer = new Serializer<Serializable>() {
+	public static final Serializer<Serializable> serializer = new Serializer<Serializable>() {
+		private static final String TYPE = "type";
+		private static final String OBJECT = "object";
+		
 		@Override
-		public Serializable read(Tree in) {
+		public Serializable<?> read(Tree in) {
 			return null;
 		}
 
-		@SuppressWarnings("unchecked")
+		@SuppressWarnings({ "unchecked" })
 		@Override
 		public Tree write(Serializable in) {
-			return in.serializer().write(in);
+			Map<String, Tree> header = new HashMap<String, Tree>();
+			header.put(TYPE, new Tree(in.getClass().getCanonicalName()));
+			header.put(OBJECT, in.serializer().write(in));
+			return new Serializers.Map<String, Tree>(Serializers.Serializer_String, Serializers.Serializer_Tree).write(header);
 		}
 	};
 
