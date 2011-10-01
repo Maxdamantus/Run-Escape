d66b6b22a9eb3a3b5c0092918c91c71f76260710
Max Zerzouri <maxdamantus@gmail.com>
2011-10-01 00:50:08 +0000

    Made WorldDelta interface hopefully saner.

--- game/WorldDelta.java
+++ game/WorldDelta.java
@@ -5,41 +5,43 @@ import serialization.*;
 public class WorldDelta {
 	public static interface Action {
 		public void apply(GameWorld w);
-		public Tree toTree(GameWorld w);
+		public Tree toTree();
 		public String type();
 	}
 
 	public static class Put implements Action {
-		private final int gid;
-		private final Location loc;
+		private final Tree in;
 
 		public Put(int g, Location l){
-			gid = g; loc = l;
+			in = new Tree();
+			in.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(g)));
+			in.add(new Tree.Entry("location", LocationS.s(null).write(l)));
+		}
+
+		public Put(Tree t){
+			in = t;
 		}
 
 		public void apply(GameWorld world){
+			int gid = Serializers.Serializer_Integer.read(in.find("gid"));
+			Location loc = LocationS.s(null).read(in.find("location"));
 			loc.put(world.thingWithGID(gid));
 		}
 
-		public static Serializer<Put> serializer(final GameWorld w){
+		public static Serializer<Put> serializer(){
 			return new Serializer<Put>(){
 				public Tree write(/* Vladimir */ Put in){
-					Tree out = new Tree();
-					out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
-					out.add(new Tree.Entry("location", LocationS.s(w).write(in.loc)));
-					return out;
+					return in.in;
 				}
 
 				public Put read(Tree in){
-					return new Put(
-						Serializers.Serializer_Integer.read(in.find("gid")),
-						LocationS.s(w).read(in.find("location")));
+					return new Put(in);
 				}
 			};
 		}
 
-		public Tree toTree(GameWorld w){
-			return serializer(w).write(this);
+		public Tree toTree(){
+			return in;
 		}
 
 		public String type(){
@@ -52,32 +54,36 @@ public class WorldDelta {
 	// $ grep -r 'public' $(find . -name \*.java) | wc -l
 	// 369
 	public static class Introduce implements Action {
-		private final int gid;
+		private final Tree in;
 
 		public Introduce(int g){
-			gid = g;
+			in = new Tree();
+			in.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(g)));
+		}
+
+		public Introduce(Tree t){
+			in = t;
 		}
 
 		public void apply(GameWorld world){
+			int gid = Serializers.Serializer_Integer.read(in.find("gid"));
 			new DumbGameThing(world, gid);
 		}
 
-		public static Serializer<Introduce> serializer(final GameWorld w){
+		public static Serializer<Introduce> serializer(){
 			return new Serializer<Introduce>(){
 				public Tree write(Introduce in){
-					Tree out = new Tree();
-					out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
-					return out;
+					return in.in;
 				}
 
 				public Introduce read(Tree in){
-					return new Introduce(Serializers.Serializer_Integer.read(in.find("gid")));
+					return new Introduce(in);
 				}
 			};
 		}
 
-		public Tree toTree(GameWorld w){
-			return serializer(w).write(this);
+		public Tree toTree(){
+			return in;
 		}
 
 		public String type(){
@@ -86,32 +92,36 @@ public class WorldDelta {
 	}
 
 	public static class Forget implements Action {
-		private final int gid;
+		private final Tree in;
 
 		public Forget(int g){
-			gid = g;
+			in = new Tree();
+			in.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(g)));
+		}
+
+		public Forget(Tree t){
+			in = t;
 		}
 
 		public void apply(GameWorld world){
+			int gid = Serializers.Serializer_Integer.read(in.find("gid"));
 			new DumbGameThing(world, gid);
 		}
 
-		public static Serializer<Forget> serializer(final GameWorld w){
+		public static Serializer<Forget> serializer(){
 			return new Serializer<Forget>(){
 				public Tree write(Forget in /* what you saw */){
-					Tree out = new Tree();
-					out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
-					return out;
+					return in.in;
 				}
 
 				public Forget read(Tree in){
-					return new Forget(Serializers.Serializer_Integer.read(in.find("gid")));
+					return new Forget(in);
 				}
 			};
 		}
 
-		public Tree toTree(GameWorld w){
-			return serializer(w).write(this);
+		public Tree toTree(){
+			return in;
 		}
 
 		public String type(){
@@ -120,31 +130,36 @@ public class WorldDelta {
 	}
 
 	public static class Update implements Action {
-		private final DumbGameThing dgt;
+		private final Tree in;
 
 		public Update(DumbGameThing d){
-			dgt = d;
+			in = DumbGameThing.serializer(d.world()).write(d);
+		}
+
+		public Update(Tree t){
+			in = t;
 		}
 
 		public void apply(GameWorld world){
+			DumbGameThing dgt = DumbGameThing.serializer(world).read(in);
 			// assumptions ..
 			((DumbGameThing)world.thingWithGID(dgt.gid())).update(dgt);
 		}
 
-		public static Serializer<Update> serializer(final GameWorld w){
+		public static Serializer<Update> serializer(){
 			return new Serializer<Update>(){
 				public Tree write(Update in){
-					return DumbGameThing.serializer(w).write(in.dgt);
+					return in.in;
 				}
 
 				public Update read(Tree in){
-					return new Update(DumbGameThing.serializer(w).read(in));
+					return new Update(in);
 				}
 			};
 		}
 
-		public Tree toTree(GameWorld w){
-			return serializer(w).write(this);
+		public Tree toTree(){
+			return in;
 		}
 
 		public String type(){
@@ -153,45 +168,46 @@ public class WorldDelta {
 	}
 
 	public static class Say implements Action {
-		private final String what;
-		private final int gid;
+		private final Tree in;
 
 		public Say(int g, String w){
-			gid = g;
-			what = w;
+			in = new Tree();
+			in.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(g)));
+			in.add(new Tree.Entry("what", new Tree(w)));
+		}
+
+		public Say(Tree t){
+			in = t;
 		}
 
 		public void apply(GameWorld world){
+			int gid = Serializers.Serializer_Integer.read(in.find("gid"));
+			String what = in.find("what").value();
 			world.emitSay(world.thingWithGID(gid), what);
 		}
 
 		public int who(){
-			return gid;
+			return Serializers.Serializer_Integer.read(in.find("gid"));
 		}
 
 		public String what(){
-			return what;
+			return in.find("what").value();
 		}
 
-		public final static Serializer<Say> serializer(final GameWorld w){
+		public final static Serializer<Say> serializer(){
 			return new Serializer<Say>(){
 				public Tree write(Say in){
-					Tree out = new Tree();
-					out.add(new Tree.Entry("gid", Serializers.Serializer_Integer.write(in.gid)));
-					out.add(new Tree.Entry("what", new Tree(in.what)));
-					return out;
+					return in.in;
 				}
 
 				public Say read(Tree in){
-					return new Say(
-						 Serializers.Serializer_Integer.read(in.find("gid")),
-						 in.find("what").value());
+					return new Say(in);
 				}
 			};
 		}
 
-		public Tree toTree(GameWorld w){
-			return serializer(w).write(this);
+		public Tree toTree(){
+			return in;
 		}
 
 		public String type(){
@@ -209,30 +225,28 @@ public class WorldDelta {
 		action.apply(w);
 	}
 
-	public final static Serializer<WorldDelta> serializer(final GameWorld w){
-		return new Serializer<WorldDelta>(){
-			public Tree write(WorldDelta in){
-				Tree out = new Tree();
-				out.add(new Tree.Entry("type", new Tree(in.action.type())));
-				out.add(new Tree.Entry("action", in.action.toTree(w)));
-				return out;
-			}
+	public final static Serializer<WorldDelta> SERIALIZER = new Serializer<WorldDelta>(){
+		public Tree write(WorldDelta in){
+			Tree out = new Tree();
+			out.add(new Tree.Entry("type", new Tree(in.action.type())));
+			out.add(new Tree.Entry("action", in.action.toTree()));
+			return out;
+		}
 
-			public WorldDelta read(Tree in){
-				Reader<? extends Action> as = null;
-				String type = in.find("type").value();
-				if(type.equals("put"))
-					as = Put.serializer(w);
-				else if(type.equals("introduce"))
-					as = Introduce.serializer(w);
-				else if(type.equals("forget"))
-					as = Forget.serializer(w);
-				else if(type.equals("update"))
-					as = Update.serializer(w);
-				else if(type.equals("say"))
-					as = Say.serializer(w);
-				return new WorldDelta(as.read(in.find("action")));
-			}
-		};
-	}
+		public WorldDelta read(Tree in){
+			Reader<? extends Action> as = null;
+			String type = in.find("type").value();
+			if(type.equals("put"))
+				as = Put.serializer();
+			else if(type.equals("introduce"))
+				as = Introduce.serializer();
+			else if(type.equals("forget"))
+				as = Forget.serializer();
+			else if(type.equals("update"))
+				as = Update.serializer();
+			else if(type.equals("say"))
+				as = Say.serializer();
+			return new WorldDelta(as.read(in.find("action")));
+		}
+	};
 }
