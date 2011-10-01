6d4c8c70dbbb8ed173875fb8643d944f32051207
Melby Ruarus <melby.ruarus@gmail.com>
2011-10-01 02:25:45 +0000

    Removed initial squares from WorldBuilder

--- ui/isometric/builder/WorldBuilderMain.java
+++ ui/isometric/builder/WorldBuilderMain.java
@@ -2,15 +2,11 @@ package ui.isometric.builder;
 
 import game.GameWorld;
 import ui.isometric.mock.ClientMessageHandlerMock;
-import util.*;
 
 public class WorldBuilderMain {
 	private static GameWorld sgm = new GameWorld();
 
 	public static void main(String[] args) {
-		game.GameThing tile = new game.things.GroundTile(sgm);
-		sgm.level(0).location(new Position(5, 0), Direction.NORTH).put(tile);
-		sgm.level(0).location(new Position(5, 1), Direction.NORTH).put(new game.things.GroundTile(sgm, "ground_grey_water_two_sides"));
 		IsoInterfaceWorldBuilder view = new IsoInterfaceWorldBuilder("World Builder", sgm, new ClientMessageHandlerMock());
 		view.show();
 	}
