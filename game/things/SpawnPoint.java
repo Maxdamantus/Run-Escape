ab89e4adcb6452b367820b1946a2478c6d432a10
Max Zerzouri <maxdamantus@gmail.com>
2011-10-02 00:31:09 +0000

    Forgot the SpawnPoint class

---
+++ game/things/SpawnPoint.java
@@ -0,0 +1,40 @@
+package game.things;
+
+import game.*;
+
+import serialization.*;
+import util.Direction;
+
+public class SpawnPoint extends AbstractGameThing {
+	public static void makeSerializer(SerializerUnion<GameThing> union, final GameWorld world){
+		union.addIdentifier(new SerializerUnion.Identifier<GameThing>(){
+			public String type(GameThing g){
+				return g instanceof Wall? "spawnpoint" : null;
+			}
+		});
+
+		union.addSerializer("wall", new Serializer<GameThing>(){
+			public Tree write(GameThing o){
+				return new Tree();
+			}
+
+			public GameThing read(Tree in){
+				SpawnPoint r = new SpawnPoint();
+				world.addSpawnPoint(r);
+				return r;
+			}
+		});
+	}
+	
+	private SpawnPoint(){
+		super(null);
+	}
+	
+	public String renderer(){
+		return "spawnpoint";
+	}
+
+	public String name(){
+		return "Spawn Point";
+	}
+}
