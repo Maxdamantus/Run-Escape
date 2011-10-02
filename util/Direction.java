1d335501d9449d311f1ee26f9ea2ded68cf1ae22
Max Zerzouri <maxdamantus@gmail.com>
2011-10-02 09:13:56 +0000

    Fixed Direction to work with Melby's concept of NESW.

--- util/Direction.java
+++ util/Direction.java
@@ -3,7 +3,7 @@ package util;
 import serialization.*;
 
 public enum Direction {
-	NORTH(0, -1), EAST(1, 0), SOUTH(0, 1), WEST(-1, 0);
+	NORTH(0, 1), EAST(1, 0), SOUTH(0, -1), WEST(-1, 0);
 
 	private int dx, dy;
 
