742364886ff47f57ae80bb82bb7e984a8e77ca0d
Melby Ruarus <melby.ruarus@gmail.com>
2011-10-03 03:55:43 +0000

    Added javadoc

--- ui/isometric/IsoAnimatedImage.java
+++ ui/isometric/IsoAnimatedImage.java
@@ -4,14 +4,33 @@ import java.awt.image.BufferedImage;
 
 import ui.isometric.IsoRendererLibrary.RendererImage;
 
+/**
+ * An animated IsoImage
+ * 
+ * @author melby
+ *
+ */
 public class IsoAnimatedImage extends IsoImage {
 	private int animationStep;
 	
+	/**
+	 * Create a standard IsoImage
+	 * @param rendererImage
+	 * @param square
+	 * @param yoff
+	 */
 	public IsoAnimatedImage(RendererImage rendererImage, IsoSquare square, int yoff) {
 		super(rendererImage, square, yoff);
 		animationStep = 0;
 	}
 
+	/**
+	 * Create an IsoAnimatedImage with a given frame of the animation
+	 * @param rendererImage
+	 * @param square
+	 * @param yoff
+	 * @param animationStep
+	 */
 	public IsoAnimatedImage(RendererImage rendererImage, IsoSquare square, int yoff, int animationStep) {
 		super(rendererImage, square, yoff);
 		this.animationStep = animationStep;
