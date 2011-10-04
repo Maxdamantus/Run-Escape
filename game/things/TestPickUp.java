7ef3327ad3494f06197a979ddc3102d61ddadb7d
wheelemaxw <wheelemaxw@ecs.vuw.ac.nz>
2011-10-04 04:00:13 +0000

    Item for testing picking up

---
+++ game/things/TestPickUp.java
@@ -0,0 +1,53 @@
+package game.things;
+
+import java.util.List;
+
+import util.Direction;
+import game.Container;
+import game.GameThing;
+import game.GameWorld;
+import game.PickupGameThing;
+
+public class TestPickUp extends PickupGameThing {
+
+	private final String renderer;
+	
+
+	
+	//For reading in (serializer to be completed)
+	public TestPickUp(GameWorld world, String name, Container cont){
+		super(world);
+		renderer = name;
+		update();
+	}
+	
+	//For empty chest
+	public TestPickUp(GameWorld world, String name){
+		super(world);
+		renderer = name;
+		update();
+	}
+	
+	//need the default renderer for a Test object, at the moment looks like a wall...
+	public TestPickUp(GameWorld world){
+		this(world, "wall_brown_1_x");
+	}
+	
+	public List<String> interactions(){
+		return interactions;
+	}
+	
+	public String renderer(){
+		return renderer;
+	}
+
+	public String name(){
+		return "TestPickUp";
+	}
+	
+	@Override
+	public boolean canWalkInto(Direction d, Player p) {
+		return true;
+	}
+}
+
