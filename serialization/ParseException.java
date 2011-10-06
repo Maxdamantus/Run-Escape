76df0cb882f03e03f43459757342e5ab43892556
Melby Ruarus <melby.ruarus@gmail.com>
2011-10-06 09:33:46 +0000

    Just to get the ball rolling...

---
+++ serialization/ParseException.java
@@ -0,0 +1,11 @@
+package serialization;
+
+/**
+ * Exception for parsing errors
+ * 
+ * @author melby
+ *
+ */
+public class ParseException extends IllegalArgumentException {
+	private static final long serialVersionUID = 1L;
+}
