c2fbecc666751d8422e1c31f8b5613aff7e97abb
Max Zerzouri <maxdamantus@gmail.com>
2011-10-08 03:58:25 +0000

    Made Stackable things merge when they're put in the same place as another of the same type of stackable thing.

--- game/things/Stackable.java
+++ game/things/Stackable.java
@@ -9,6 +9,17 @@ public abstract class Stackable extends AbstractGameThing {
 
 	abstract public Object type();
 
+	public Location location(Location l){
+		if(l != LocationS.NOWHERE)
+			for(GameThing g : l.contents())
+				if(g instanceof Stackable && ((Stackable)g).merge(this)){
+					LocationS.NOWHERE.put(this);
+					forget();
+					return super.location(LocationS.NOWHERE);
+				}
+		return super.location(l);
+	}
+
 	public Stackable(GameWorld w, int a){
 		super(w);
 		amount = a;
@@ -24,6 +35,14 @@ public abstract class Stackable extends AbstractGameThing {
 
 	public int amount(int s){
 		return amount = s;
+	}
+
+	public boolean merge(Stackable s){
+		if(s.type() == type()){
+			amount(amount() + s.amount());
+			return true;
+		}
+		return false;
 	}
 
 	public Map<String, String> info(){
