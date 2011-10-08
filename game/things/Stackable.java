821fd412d322111be25ffa0533c5e00b5afbc67a
Max Zerzouri <maxdamantus@gmail.com>
2011-10-08 03:03:12 +0000

    Added a Stackable base class. A    Stackable.java

---
+++ game/things/Stackable.java
@@ -0,0 +1,32 @@
+package game.things;
+
+import game.*;
+
+import java.util.*;
+
+public abstract class Stackable extends AbstractGameThing {
+	private int amount;
+
+	public Stackable(GameWorld w, int a){
+		super(w);
+		amount = a;
+	}
+
+	public Stackable(GameWorld w){
+		this(w, 1);
+	}
+
+	public int amount(){
+		return amount;
+	}
+
+	public int amount(int s){
+		return amount = s;
+	}
+
+	public Map<String, String> info(){
+		Map<String, String> out = new HashMap<String, String>(super.info());
+		out.put("stackcount", String.valueOf(amount));
+		return out;
+	}
+}
