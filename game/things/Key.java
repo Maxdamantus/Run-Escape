faf727b281543d1212a598b8d2f08ef3f41f5269
wheelemaxw <wheelemaxw@ecs.vuw.ac.nz>
2011-10-10 01:49:25 +0000

    Serializer added

--- game/things/Key.java
+++ game/things/Key.java
@@ -1,15 +1,44 @@
 package game.things;
 
 import game.Container;
+import game.GameThing;
 import game.GameWorld;
 import game.Location;
-import game.things.EquipmentGameThing.Slot;
 
 import java.util.LinkedList;
 import java.util.List;
 
-public class Key extends PickupGameThing {
+import serialization.ParseException;
+import serialization.Serializer;
+import serialization.SerializerUnion;
+import serialization.Serializers;
+import serialization.Tree;
 
+public class Key extends PickupGameThing {
+	public static void makeSerializer(SerializerUnion<GameThing> union, final GameWorld world){
+		union.addIdentifier(new SerializerUnion.Identifier<GameThing>(){
+			public String type(GameThing g){
+				return g instanceof Key? "Key" : null;
+			}
+		});
+
+		union.addSerializer("Key", new Serializer<GameThing>(){
+			public Tree write(GameThing o){
+				Key in = (Key)o;
+				Tree out = new Tree();
+				out.add(new Tree.Entry("renderer", new Tree(in.renderer)));
+				out.add(new Tree.Entry("doorcode", new Tree(in.renderer)));
+				return out;
+			}
+
+			public GameThing read(Tree in) throws ParseException {
+				return new Key(world,
+					in.find("renderer").value(),
+					in.find("doorcode").value());
+			}
+		});
+	}
+	
 	private String name, renderer, doorcode;
 	
 	public Key(GameWorld w, String ren, String drcd){
