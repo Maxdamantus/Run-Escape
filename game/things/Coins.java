b67a4fb111a637a1f74b551ea6d55ad273a25b7f
wheelemaxw <wheelemaxw@ecs.vuw.ac.nz>
2011-10-12 02:10:24 +0000

    JavaDoc

--- game/things/Coins.java
+++ game/things/Coins.java
@@ -7,7 +7,18 @@ import serialization.Serializers;
 import serialization.Tree;
 import game.*;
 
+/**
+ * Instance of a Stackable item to represent Money in the GameWorld
+ * @author zerzoumax and wheelemaxw
+ *
+ */
+
 public class Coins extends Stackable {
+	/**
+	 * Custom serializer for Coin or Coins
+	 * @param union
+	 * @param world
+	 */
 	public static void makeSerializer(SerializerUnion<GameThing> union, final GameWorld world){
 		union.addIdentifier(new SerializerUnion.Identifier<GameThing>(){
 			public String type(GameThing g){
@@ -36,18 +47,29 @@ public class Coins extends Stackable {
 		super(w, amt);
 	}
 
+	/**
+	 * @return returns an Object
+	 */
 	public Object type(){
 		return type;
 	}
 
+	/**
+	 * Changes name dependant on amount
+	 * @return name
+	 */
 	public String name(){
 		return amount() != 1? "Coins" : "Coin";
 	}
 
+	/**
+	 * Static renderer for coins
+	 */
 	public String renderer(){
 		return "coins_gold";
 	}
 
+	@Override
 	public int renderLevel(){
 		return ui.isometric.abstractions.IsoSquare.DROP;
 	}
