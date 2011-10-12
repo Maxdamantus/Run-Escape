47fa66a9dfdf69c99d5fa2e468f735a2ef99eb6b
wheelemaxw <wheelemaxw@ecs.vuw.ac.nz>
2011-10-12 06:45:13 +0000

    Serializer

--- game/things/Light.java
+++ game/things/Light.java
@@ -4,12 +4,48 @@ import game.*;
 
 import java.util.*;
 
+import serialization.ParseException;
+import serialization.Serializer;
+import serialization.SerializerUnion;
+import serialization.Serializers;
+import serialization.Tree;
+
 /**
  * 
  * @author zerzoumax
  *
  */
 public class Light extends AbstractGameThing implements Luminant {
+	/**
+	 * Custom serializer for Light
+	 * @param union
+	 * @param world
+	 */
+	public static void makeSerializer(SerializerUnion<GameThing> union, final GameWorld world){
+		union.addIdentifier(new SerializerUnion.Identifier<GameThing>(){
+			public String type(GameThing g){
+				return g instanceof Light? "Light" : null;
+			}
+		});
+
+		union.addSerializer("Light", new Serializer<GameThing>(){
+			public Tree write(GameThing o){
+				Light in = (Light)o;
+				Tree out = new Tree();
+				out.add(new Tree.Entry("renderer", new Tree(in.renderer)));
+				out.add(new Tree.Entry("luminance", Serializers.Serializer_Integer.write(in.luminance())));
+				return out;
+			}
+
+			public GameThing read(Tree in) throws ParseException {
+				return new Light(world,
+					in.find("renderer").value(),
+					Serializers.Serializer_Integer.read(in.find("luminance")));
+			}
+		});
+	}
+	
+	
 	private int luminance;
 	private final String renderer;
 
